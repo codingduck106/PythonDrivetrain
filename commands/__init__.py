3980cb8f3ab5808623a7aa5fa252a1be282a85f8
codingduck106 <jazl.ihsan@gmail.com>
2025-09-27 18:34:54 -0700

    deleted unused files

--- commands/__init__.py
+++
@@ -1,6 +0,0 @@
-# what is the purpose of these __init__.py files?
-# So we can do stuff like:
-# from commands import DefaultDriveCommand
-# or something like that idk
-
-from commands.defaultdrivecommand import *
--- subsystems/__init__.py
+++
@@ -1,7 +0,0 @@
-# what is the purpose of these __init__.py files?
-# So we can do stuff like:
-# from subsystems import Drive
-# or something like that idk
-
-from subsystems.drivetrain import *
-from subsystems.simdrivetrain import *

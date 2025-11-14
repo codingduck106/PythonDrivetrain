f3491995c52865aff738b06114a037d381e3d2d1
codingduck106 <jazl.ihsan@gmail.com>
2025-11-13 22:59:38 -0800

    added drivetrain setup and restarted progress with proper checks

--- constants.py
+++ constants.py
@@ -1,30 +1,196 @@
 from commands2.button import CommandPS4Controller
+from phoenix6.units import volt, rotation, degree, inch, meters_per_second, rotations_per_second, ampere
+from phoenix6.configs import Slot0Configs
+from pathplannerlib.config import PIDConstants
 
-# This is just the constants the robot uses.
+
+class GenericConstants:
+    # WARNING: The mechanisms and the drivetrain are on separate CAN loops, but DO NOT reuse CAN IDs.
+    # Can ID reuse will mess with simulation.
+
+    # Drivetrain Constants
+
+    # CAN Loop identifier for swerve drive devices (motors and encoders).
+    DRIVE_CAN_LOOP_NAME = "drive"
+
+    # Drivetrain CAN IDs
+
+    # CAN ID for front right steer motor.
+    FRONT_RIGHT_STEER_ID = 1
+
+    # CAN ID for front right encoder.
+    FRONT_RIGHT_ENCODER_ID = 2
+
+    # CAN ID for front right drive motor.
+    FRONT_RIGHT_DRIVE_ID = 3
+
+    # CAN ID for back right steer motor.
+    BACK_RIGHT_STEER_ID = 4
+
+    # CAN ID for back right encoder.
+    BACK_RIGHT_ENCODER_ID = 5
+
+    # CAN ID for back right drive motor.
+    BACK_RIGHT_DRIVE_ID = 6
+
+    # CAN ID for back left steer motor.
+    BACK_LEFT_STEER_ID = 7
+
+    # CAN ID for back left encoder.
+    BACK_LEFT_ENCODER_ID = 8
+
+    # CAN ID for back left drive motor.
+    BACK_LEFT_DRIVE_ID = 9
+
+    # CAN ID for front left steer motor.
+    FRONT_LEFT_STEER_ID = 10
+
+    # CAN ID for front left encoder.
+    FRONT_LEFT_ENCODER_ID = 11
+
+    # CAN ID for front left drive motor.
+    FRONT_LEFT_DRIVE_ID = 12
+
+    # Inertial Measurement Unit (IMU) CAN ID.
+    PIGEON_ID = 13
+
+    # Mechanism Constants.
+
+    # CAN Loop identifier for non-drive devices.
+    MECHANISM_CAN_LOOP_NAME = "rio"  # TODO: See if we can change this to "mech"
+
+    # Ground Intake Can IDs
+
+    # CAN ID for ground intake pivot motor.
+    GROUND_INTAKE_PIVOT_ID = 21
+
+    # CAN ID for ground intake wheel motor.
+    GROUND_INTAKE_WHEEL_ID = 22
+
+    # Elevator and Arm Can IDs
+
+    # CAN ID for the elevator motor
+    ELEVATOR_MOTOR_ID = 23
+
+    # CAN ID for the arm pivot encoder.
+    ARM_PIVOT_ENCODER_ID = 24
+
+    # CAN ID for the arm pivot motor.
+    ARM_PIVOT_MOTOR_ID = 25
+
+    # CAN ID for the arm grabber (intake) motor.
+    ARM_WHEEL_MOTOR_ID = 26
+
+    # CAN ID for the CAN range sensor used by the arm wheels.
+    CAN_RANGE_ID = 27
+
+    class DriverConstants:
+        # Pre-set driver controller instance.
+        DRIVER_CONTROLLER = CommandPS4Controller(0)
+
+    class OperatorConstants:
+    # Pre-set operator controller instance.
+        OPERATOR_CONTROLLER = CommandPS4Controller(1)
 
 
-class FLConstants:
-    DRIVE = 12
-    TURN = 10
-    CAN = 11
+    # Triggers for elevator and arm positions
+    CORAL_GROUND_TRIGGER = (
+        DriverConstants.DRIVER_CONTROLLER.L2()
+        or OperatorConstants.OPERATOR_CONTROLLER.L2()
+    )  # also used by ground intake
+    OUTTAKE_TRIGGER = (
+        DriverConstants.DRIVER_CONTROLLER.R2() or OperatorConstants.OPERATOR_CONTROLLER.R2()
+    )
+    CORAL_L4_TRIGGER = OperatorConstants.OPERATOR_CONTROLLER.triangle()
+    CORAL_L3_TRIGGER = OperatorConstants.OPERATOR_CONTROLLER.square()
+    CORAL_L2_TRIGGER = OperatorConstants.OPERATOR_CONTROLLER.circle()
+    CORAL_L1_TRIGGER = OperatorConstants.OPERATOR_CONTROLLER.cross()
+    BARGE_TRIGGER = OperatorConstants.OPERATOR_CONTROLLER.povUp()
+    ALGAE_L3_TRIGGER = OperatorConstants.OPERATOR_CONTROLLER.povLeft()
+    ALGAE_L2_TRIGGER = OperatorConstants.OPERATOR_CONTROLLER.povRight()
+    ALGAE_GROUND_TRIGGER = OperatorConstants.OPERATOR_CONTROLLER.povDown()
+    WILD_CARD = OperatorConstants.OPERATOR_CONTROLLER.PS()
 
-class FRConstants:
-    DRIVE = 3
-    TURN = 1
-    CAN = 2
+    ALGAE_LOLLIPOP_TRIGGER = OperatorConstants.OPERATOR_CONTROLLER.share()
 
-class BLConstants:
-    DRIVE = 9
-    TURN = 7
-    CAN = 8
+class ArmConstants:
+    ARM_INTAKE_VOLTAGE = volt(5)
+    ARM_OUTTAKE_VOLTAGE = volt(-3)
 
-class BRConstants:
-    DRIVE = 6
-    TURN = 4
-    CAN = 5
+    ACCEPTABLE_ERROR_DEGREES = degree(1.0)
 
-GYRO = 13
+    # PID Constants
+    ARM_PIVOT_P = 61
+    ARM_PIVOT_I = 2
+    ARM_PIVOT_D = 0
 
-DRIVER_CONTROLLER = CommandPS4Controller(0)
-SIM_STEER_CONTROLER = CommandPS4Controller(1)
-RESET_POSE = DRIVER_CONTROLLER.triangle()
+class ElevatorConstants:
+
+    # PID Constants
+    ELEVATOR_P = 1.08
+    ELEVATOR_I = 0.02
+    ELEVATOR_D = 0.13
+
+    ACCEPTABLE_ERROR_ROTATIONS = rotation(0.1)
+
+class GroundIntakeConstants:
+    PIVOT_UP_POSITION_ROTATIONS = rotation(-0.25)
+    PIVOT_DOWN_POSITION_ROTATIONS = rotation(0.25)
+
+    ACCEPTABLE_ERROR = 0.1
+
+    PIVOT_P = 1
+    PIVOT_I = 0
+    PIVOT_D = 0
+
+    INTAKE_VOLTAGE = volt(9)
+
+    OUTTAKE_VOLTAGE = volt(-9)
+
+class DriveConstants:
+    DISTANCE_FROM_BOT_CENTER_TO_FRONT = inch(11.2375)
+
+    DISTANCE_FROM_BOT_CENTER_TO_LEFT = inch(11.2375)
+
+    FRONT_LEFT_ENCODER_OFFSET = rotation(0.04492)
+
+    FRONT_RIGHT_ENCODER_OFFSET = rotation(0.470215)
+
+    BACK_LEFT_ENCODER_OFFSET = rotation(0.478516)
+
+    BACK_RIGHT_ENCODER_OFFSET = rotation(0.497314)
+
+    DRIVE_MOTOR_GEAR_RATIO = 6.75
+
+    STEER_MOTOR_GEAR_RATIO = 150/7
+
+    WHEEL_RADIUS = inch(3.5/2)
+
+    MAX_LINEAR_VELOCITY = meters_per_second(4)
+
+    MAX_ANGULAR_VELOCITY = rotations_per_second(1.5)
+
+    MOTOR_SLIP_CURRENT = ampere(90)
+
+    COUPLING_GEAR_RATIO = 3.5
+
+    DRIVE_PID = (Slot0Configs()
+                 .with_k_p(0.1)
+                 .with_k_i(0)
+                 .with_k_d(0)
+                 .with_k_s(0)
+                 .with_k_v(0.124)
+                 .with_k_a(0))
+    
+    STEER_PID = (Slot0Configs()
+                 .with_k_p(100)
+                 .with_k_i(0)
+                 .with_k_d(0.5)
+                 .with_k_s(0.1)
+                 .with_k_v(2.66)
+                 .with_k_a(0))
+
+
+    TRANSLATION_AUTO_PID = PIDConstants(5,0,0)
+
+    ROTATION_AUTO_PID = PIDConstants(5,0,0)
--- robotcontainer.py
+++ robotcontainer.py
@@ -1,101 +1,21 @@
-from pathplannerlib.config import RobotConfig, PIDConstants
-from pathplannerlib.auto import AutoBuilder, SendableChooser
-from pathplannerlib.controller import PPHolonomicDriveController
-from wpimath.units import meters
-from subsystems import Drive
-from wpilib import RobotBase
-from wpimath.geometry import Pose2d, Rotation2d
-import wpilib
-from commands2 import Command
+from ntcore import NetworkTableInstance
 from wpilib import SmartDashboard
-from commands2.instantcommand import InstantCommand
-from commands import DefaultDriveCommand
-from constants import *
-import math
-import typing
-
-DEFAULT_MAX_VELOCITY_METERS_PER_SECOND = 6
-DEFAULT_MAX_ROTATIONS_PER_SECOND = 1.2
-max_radians_per_second = DEFAULT_MAX_ROTATIONS_PER_SECOND * math.pi * 2
-
+from commands2 import Command
+from pathplannerlib.auto import AutoBuilder
 
 class RobotContainer:
-    """BOT CONTAINER FOR COMMANDS!!!!!!"""
 
-    ALLIANCE_USED_IN_PATHS= wpilib.DriverStation.Alliance.kBlue
-    
-    bot_pose_blue_origin = Pose2d(meters(-8.7736), meters(-4.0257), Rotation2d())
-    def __init__(self):
-        """Initializes the Robot Container, which is a container containing every subsystem the bot has."""
-        self.drive = Drive()  # Use the fixed drivetrain
-        self.drive.setDefaultCommand(
-            DefaultDriveCommand(self.drive,
-                                lambda: -Drive.modifyAxis(DRIVER_CONTROLLER.getLeftY()) * DEFAULT_MAX_VELOCITY_METERS_PER_SECOND, # SEE, LAMBDA FUNCTIONS! I TOLD YOU! (if you don't understand, check defaultdrivecommand.py under the commands folder)
-                                lambda: -Drive.modifyAxis(DRIVER_CONTROLLER.getLeftX()) * DEFAULT_MAX_VELOCITY_METERS_PER_SECOND,
-                                lambda: -Drive.modifyAxis(DRIVER_CONTROLLER.getRightX() if not RobotBase.isSimulation() else SIM_STEER_CONTROLER.getLeftX()) * max_radians_per_second) # added simulation support
-        )
+    def __init__(self, ntInstance: NetworkTableInstance):
 
-        self.configureBindings() # configures the controller bindings for the subsystems.
-        
-        self.autoChooser = self.configure_auto(self.drive) # configures the auto.
-        SmartDashboard.putData("Auto Routine", self.autoChooser) # adds an autochooser to SmartDashboard
+        self.autopath_dropdown = AutoBuilder.buildAutoChooser()
 
-    def configure_auto(self, drive: Drive) -> SendableChooser: # add more subsystems later
-        """Configures the auto. We initialize Pathplanner's auto stuff here.
-        
-        Usually contains other subsystems, but we only have drivetrain.
-        
-        :param drive: the drivetrain instance.
-        :returns: The autochooser."""
-        try:
-            config = RobotConfig.fromGUISettings() # gets the Robot config from a json file
-        except Exception as e:
-            raise RuntimeError("Could not get Config", e)
-        
-        AutoBuilder.configure( # AUTOBUILDER CONFIGURATION RAHH
-                drive.get_pose,
-                drive.set_pose,
-                drive.get_robot_relative_speeds,
-                lambda speeds, ff: drive.driveRobotRelative(speeds),  # We use a lambda here because pathplanner also gives us feedforwards, but we don't use feedforwards.
-                PPHolonomicDriveController(
-                    PIDConstants(15.0, 0.0, 0.0),
-                    PIDConstants(6.85, 0.0, 1.3),
-                ),#
-                config,
-                lambda: wpilib.DriverStation.getAlliance() != self.ALLIANCE_USED_IN_PATHS, # whether pathplanner should flip the autopaths or not depending on which alliance we're on. True if we're on red.
-                drive,
-        )
-        self.configure_auto_commands() # add subsystems as arguments later
-        return AutoBuilder.buildAutoChooser("blue-left-leave")
-    
-    @classmethod
-    def to_bot_pose_blue(cls, orig: Pose2d) -> Pose2d:
-        """In Dr. Womp, this is a static method.
-        
-        We use class method here because we want to actually modify the class variable.
-        
-        :param orig: the origin of the field.
-        :returns: a `Pose2d` object containing the current pose relative to the origin."""
-        return orig.relativeTo(cls.bot_pose_blue_origin)
-    
-    
-    def configure_auto_commands(self) -> None:
-        """We add other subsystems here. They have auto commands too!"""
-        pass # leave empty for now, add subsystem functions later
-    
-    def configureBindings(self) -> None:
-        """Configures the controller bindings. So far, we only have drive, which is this."""
-        if not RobotBase.isSimulation():
-            RESET_POSE.onTrue(InstantCommand(lambda: self.drive.reset_pose()))
-        else:
-            pass
-    
-    def getAutonomousCommand(self) -> Command | typing.Any: #================GETTER=====================
-        """Gets the auto command"""
-        return self.autoChooser.getSelected()
-    
-    def close(self): # add more subsystems later
-        """closes all the subsystems.
-        
-        so far, only drive :("""
-        self.drive.close()
+        SmartDashboard.putData("Autopath selection", self.autopath_dropdown)
+
+        self.configure_bindings()
+
+
+    def configure_bindings(self):
+        pass
+
+    def get_auton_command(self) -> Command:
+        return self.autopath_dropdown.getSelected()
